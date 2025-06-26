bc220506bf5c3b106cbed0b959bd684132d4c7e4
QaisYudha-Pr <qaisprawirayudha@gmail.com>
2025-06-26 11:32:07 +0700

    bebas

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+        <div class="shadow-md p-4 justify-between flex bg-transparent  ">
+            <video autoplay muted loop playsinline
+                class="rounded-full cursor-pointer shadow-md w-10 h-10 hover:scale-105 ">
+                <source src="/zesty.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <h1 class="invisible font-lg text-center">INI PUNYA QAIS</h1>
+            <ul class="flex gap-4">
+                <li><nuxt-link to="/">
+                        <h2
+                        class=" text-gray-300 hover:text-white hover:scale-105 text-lg transition:text duration-300">
+                        Home
+                        </h2>
+                    </nuxt-link>
+                </li>
+                <li><nuxt-link to="/about">
+                        <h2
+                        class=" text-gray-300 hover:text-white hover:scale-105 text-lg transition:text duration-300">
+                        About
+                        </h2>
+                    </nuxt-link>
+                </li>
+                <li><nuxt-link to="/content">
+                        <h2
+                        class=" text-gray-300 hover:text-white hover:scale-105 text-lg transition:text duration-300">
+                        Project
+                        </h2>
+                    </nuxt-link>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+h2 {
+    font-family: Alike;
+}
+h1 {
+    font-family: Alike;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="bg-black z-[-2]">
+<div class="overflow-hidden bg-no-repeat  bg-center w-full h-screen bg-[url('/baddie.jpg')] z-[-1]">
+
+  <div class="relative w-full h-screen overflow-hidden">
+    <!-- VIDEO BACKGROUND -->
+    <video
+      autoplay
+      muted
+      loop
+      playsinline
+      preload="auto"
+      class="absolute top-0 left-0 w-full h-full object-cover z-0"
+    >
+      <source src="/hitam.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+
+    <!-- KONTEN DI ATAS VIDEO -->
+    <div class="relative z-10">
+      <HeaderComponent />
+      <div class="p-10 text-white">
+        <h1 class="text-4xl font-bold ">TEST</h1>
+        <p class="mt-2 font-light">Ini adalah halaman testing untuk deploy</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+<style scoped>
+h1 {
+    font-family: poppins;
+}
+p {
+    font-family: poppins;
+}
+</style>
